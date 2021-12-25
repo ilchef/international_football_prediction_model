9bfb8f08b1ed961aae53ad54b2bdd00bba4b4803
ilchef <thomasilchef@gmail.com>
2021-12-25 20:01:01 +1100

    finished data prep (cleaning and matching country names) needs periodic review with new data releases

---
+++ functions/standardise_countries.R
@@ -0,0 +1,25 @@
+
+standardise_countries <- function(dt,varname){
+        # dont want any capitals...
+        dt[,paste(varname) := tolower(get(varname))]
+        
+        dt[,paste(varname) := case_when(str_detect(get(varname),"brunei")~"brunei"
+                         ,str_detect(get(varname),"verde")~"capo verde"
+                         ,str_detect(get(varname),"congo dr")~"dr congo"
+                         ,str_detect(get(varname),"ivoire")~"ivory coast"
+                         ,get(varname) == "korea dpr"~"north korea"
+                         ,get(varname) == "korea republic"~"south korea"
+                         
+                         ,str_detect(get(varname),"vincent")~"saint vincent"
+                         ,str_detect(get(varname),"st\\.")~str_replace_all(get(varname),"st\\.","saint")
+                         ,str_detect(get(varname),"iran")~"iran"
+                         ,str_detect(get(varname),"kyrgyz")~"kyrgyzstan"
+                         ,str_detect(get(varname),"szã©kely land")~"swaziland"
+                         ,str_detect(get(varname),"united states")~"usa"
+                         ,str_detect(get(varname),"united states virgin islands")~"us virgin islands"
+                         ,TRUE~get(varname)
+                         )]
+
+        return(dt)
+}
+
