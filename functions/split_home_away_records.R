a9b82cb7bf32e31f92cce479ceb4e8cfb3244897
Tom Ilchef <tilchef@MBP-von-Tom.hub>
2022-11-20 23:57:07 +1100

    pmppoaegmr

--- functions/split_home_away_records.R
+++ functions/split_home_away_records.R
@@ -1,21 +1,29 @@
 split_home_away_records <- function(dt,tag,extras){
-        dt_temp <- dt %>% copy()
-        
+        dt_temp <- dt %>% copy() %>% setDT()
         name_list <- names(dt_temp)
         name_list <- name_list[str_detect(name_list,tag)|name_list %in% extras]
-        
-        dt_temp <- dt_temp[,name_list]
         
         if(tag == "away"){
                 list_h2a <- name_list[str_detect(name_list,"home2away")]
                 for(i in list_h2a){
-                        dt_temp[,paste0(list_h2a[i]) := get(list_h2a[i])*-1]
+                        dt_temp[,paste0(i) := get(i)*-1]
                 }
+                
+                dt_temp[,result := case_when(home_score > away_score ~"loss"
+                                             ,home_score == away_score ~"tie"
+                                             ,TRUE~"win")]
                         
+        } else {
+                dt_temp[,result := case_when(home_score > away_score ~"win"
+                                             ,home_score == away_score ~"tie"
+                                             ,TRUE~"loss")]
         }
+        name_list <- append(name_list,"result")
+
+        dt_temp <- dt_temp[,..name_list]
+        
         names(dt_temp) <- str_replace_all(names(dt_temp),paste0("home2away_|",tag,"_"),"")
         
-        dt_temp
+        dt_temp[,home_away_flag := tag]
+        dt_temp[,score:=NULL]
 }
-
-test <- split_home_away_records(matches_x_rankings_ts,"away",c("neutral","tournament","date"))
