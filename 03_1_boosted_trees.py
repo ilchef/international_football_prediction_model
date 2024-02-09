ad6725b347a3aa7bbc1218236915340231cccef9
Tom Ilchef <tilchef@MacBook-Pro-von-Tom.local>
2024-02-09 12:16:13 +1100

    adding ranking scraping file (requires debugging!!)

--- 03_1_boosted_trees.py
+++ 03_1_boosted_trees.py
@@ -10,19 +10,19 @@ import pandas as pd
 import sklearn as skl
 import xgboost as xgb
 
-sys.path.append("functions")
+sys.path.append("functions/py")
 import py_modules
 
 
 # Step 1: Read in Data
-cols_to_remove = ['Unnamed: 0','team','date','tournament','confederation']
+df = pd.read_csv("data/output/final_model_data__"+data_version+".csv")
 
+cols_to_remove = ['Unnamed: 0','team','date','tournament','confederation']
 df=df.drop(columns=cols_to_remove)
 
-df = pd.read_csv("data/output/final_model_data__"+data_version+".csv")
 df.head(5)
 
 # Step 2: Pre-treat Data, partition
 df = py_modules.clear_nas_python(df)
 X,y = df.drop('result',axis=1),df.result
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=seed)
+X_train, X_test, y_train, y_test = skl.train_test_split(X, y, test_size=0.3, random_state=seed)
